56465f6730473f3aeba6e3058b12c58457e7c48c
Dave Rolsky <autarch@urth.org>
2015-07-18 20:48:06 -0500

    Bump version after release

--- bin/graph-meta.pl
+++ bin/graph-meta.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Meta::Grapher::Moose;
 
--- lib/Meta/Grapher/Moose.pm
+++ lib/Meta/Grapher/Moose.pm
@@ -5,7 +5,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Getopt::Long;
 use GraphViz2;
